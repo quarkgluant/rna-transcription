1f0c22087d896504221a22e0ee4a6a4edc8bf647
Patrice Cadiot <pathibul.r@gmail.com>
2021-06-28 18:07:30 +0200

    removes solution (thanks exercism)

---
+++ lib/rna_transcription.ex
@@ -0,0 +1,11 @@
+defmodule RnaTranscription do
+  @doc """
+  Transcribes a character list representing DNA nucleotides to RNA
+
+  ## Examples
+
+  iex> RnaTranscription.to_rna('ACTG')
+  'UGAC'
+  """
+  @spec to_rna([char]) :: [char]
+end
